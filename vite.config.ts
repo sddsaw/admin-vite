af0745c56f4e652ce34a0624f43329df4e939b5f
周恩波 <zhouenbo@lx-dtx.com>
2023-05-28 17:41:23 +0800

    feat: 删除package.json type:module

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,16 @@
  * @Author: 周恩波 zhouenbo@lx-dtx.com
  * @Date: 2023-05-26 19:42:45
  * @LastEditors: 周恩波
- * @LastEditTime: 2023-05-26 21:05:17
+ * @LastEditTime: 2023-05-28 16:56:24
  * @FilePath: /admin-vite/vite.config.ts
  * @Description:
  * Copyright (c) 2023 by 上海有我科技有限公司, All Rights Reserved.
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import eslint from 'vite-plugin-eslint'
+// import eslint from 'vite-plugin-eslint'
+// , eslint()
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslint()]
+  plugins: [vue()]
 })
